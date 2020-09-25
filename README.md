8e00aa3e6e04ae590f8a6e8dd13dcd416c033be8
rong <1141591465@qq.com>
2020-09-25 20:06:58 +0800

    add more docs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sqlserver-parser
 tds  parser for sql server (just for learning)
+
+
+
+##  Thanks
+
+parser inspired by https://github.com/denisenkom/go-mssqldb
+
+
+
+
+
+
+
+
+
--- mytest/tdsheader_test.go
+++ mytest/tdsheader_test.go
@@ -1,7 +1,6 @@
 package mytest
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/rongfengliang/sqlserver-parser/pkg/packet"
@@ -9,11 +8,11 @@ import (
 
 func TestNewTdsBuffer(t *testing.T) {
 	//h := packet.Header{}
-	info := []byte{0x03, 0x04, 0x00, 0x09, 0x00, 0x00, 0x01, 0x00,0x33,0x31,0x45,0x67}
+	info := []byte{0x03, 0x04, 0x00, 0x09, 0x00, 0x00, 0x01, 0x00, 0x33, 0x31, 0x45, 0x67}
 	tds := packet.NewTdsBuffer(10, info)
 	packetype, _ := tds.BeginRead()
 	//t.Log(packetype)
-	fmt.Println("packet type:",packetype)
+	t.Log("packet type:", packetype)
 	// log.Println(info)
 	// h.PacketType = packet.PacketType(info[0])
 	// h.Length = binary.BigEndian.Uint16(info[2:4])
